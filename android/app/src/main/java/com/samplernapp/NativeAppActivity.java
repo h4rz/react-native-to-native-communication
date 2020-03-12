1e6cfe51df721449c619b729aaff9049b5c7d43f
Harsh Rajgor <harsh@keubik.com>
2020-03-12 14:34:04 +0530

    added eg for Native to Native Activity Communication

--- android/app/src/main/java/com/samplernapp/NativeAppActivity.java
+++ android/app/src/main/java/com/samplernapp/NativeAppActivity.java
@@ -2,9 +2,11 @@ package com.samplernapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import java.text.MessageFormat;
 
@@ -19,5 +21,13 @@ public class NativeAppActivity extends AppCompatActivity {
         String password = "Password : " + intent.getStringExtra("password");
         TextView tvUserNamePass = findViewById(R.id.tv_username_pass);
         tvUserNamePass.setText(MessageFormat.format("{0} {1}", userName, password));
+
+        AppCompatButton btn = findViewById(R.id.btn_eg);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NativeAppActivity.this, ExampleActivity.class));
+            }
+        });
     }
 }
